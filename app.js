ce0d9f63f614dad7635975b5b051ae5f65bbe476
baidu <baidu@MacBook-Pro.local>
2016-01-23 23:04:24 +0800

    请求返回  改为pipe  不再使用   data事件

--- app.js
+++ app.js
@@ -10,20 +10,33 @@ var path = require('path');
 var querystring = require('querystring');
 var zlib = require('zlib');
 var parse = require('co-body');
+var fileparse = require('co-busboy');
 //var route = require('./route/route.js');
 var serve = require('koa-static');
+
+
 // 返回用户目录
 function getUserHome() {
     return process.env.HOME || process.env.USERPROFILE;
 }
+
+
 var home = getUserHome();
-console.log(path.join(home,'.mock'))
-app.use(serve(path.join(home,'.mock'),{
-    index:'index.html'
+
+// 读取配置文件
+var confPath = path.join(home, '.mock/server-conf.js');
+var config = require(confPath);
+
+// 静态服务器  默认为index.html
+app.use(serve(path.join(home, '.mock'), {
+    index: 'index.html'
 }))
+
+// 未来会引入mock 假数据会用到路由
 //route(app);
 
-// static-catche 不支持index.html 改用 koa-static
+
+// static-cache 不支持index.html 改用 koa-static
 //var staticCache = require('koa-static-cache');
 //app.use(staticCache(path.join(__dirname, 'public'), {
 //    maxAge: 365 * 24 * 60 * 60,
@@ -31,15 +44,32 @@ app.use(serve(path.join(home,'.mock'),{
 //}))
 
 
+
+// 优先处理文件上传 multipart类型请求
+app.use(function * (next){
+    if(!this.request.is('multipart/*')){
+        return yield next;
+    }
+   //  var parts = fileparse(this);
+})
+
 app.use(function *(next) {
+    if(this.request.is('multipart/*')){
+        return yield next;
+    }
     var body;
     var reqUrl;
     var options = {
-        hostname: 'mbc.baidu.com',
+        hostname: '',
         port: null,
         path: '/',
         method: 'GET',
     };
+    options.hostname = config.hostname;
+    if (config.port) {
+        options.port = config.port;
+    }
+
     var data;
     var self = this;
 
@@ -49,16 +79,20 @@ app.use(function *(next) {
     options.headers.host = options.hostname;
 
     if (this.request.method === 'GET') {
+        // 还是有必要的
         data = yield new Promise(function (reslove, reject) {
             var req = http.request(options, function (res) {
                 self.res.writeHead(res.statusCode, res.headers);
-                res.on('data', function (data) {
-                    self.res.write(data);
-                })
-                res.on('end', function () {
-                    self.res.end();
-                    reslove()
-                })
+
+                 res.pipe(self.res);
+
+                //res.on('data', function (data) {
+                //    self.res.write(data);
+                //})
+                //res.on('end', function () {
+                //    self.res.end();
+                //    reslove()
+                //})
             })
             req.on('error', function (error) {
                 console.log(error);
@@ -67,34 +101,59 @@ app.use(function *(next) {
         })
 
     } else if (this.request.method === 'POST') {
-        console.log(options);
         options.method = 'POST';
         //reqUrl = url.parse(this.request.url);
         var body = yield parse(this);
         var postData = querystring.stringify(body);
-        console.log(body);
         data = yield new Promise(function (reslove, reject) {
             var req = http.request(options, function (res) {
                 self.res.writeHead(res.statusCode, res.headers);
-                res.on('data', function (data) {
-                    self.res.write(data);
-                })
-                res.on('end', function () {
-                    self.res.end();
-                    reslove()
-                })
+
+                // response 和request都是 Stream  遂用pipe来代替事件机制
+                res.pipe(self.res);
+
+                //res.on('data', function (data) {
+                //    self.res.write(data);
+                //})
+                //res.on('end', function () {
+                //    self.res.end();
+                //    console.log(111)
+                //    reslove()
+                //})
             })
             req.on('error', function (error) {
                 console.log(error);
             });
-            req.write(postData);
-            req.end();
+            // console.log(this);
+            // console.log(this.request.pipe);
+
+             req.write(postData);
+             req.end();
         })
+        console.log(data)
     }
 });
 
 app.listen(3000);
 console.log('listen at : 3000')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 以下内容为  解析zlib
